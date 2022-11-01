469135d37cc13359daf3a0aef17db472908553a5
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-11-01 23:31:30 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,22 +13,21 @@ case "$1" in
         export TARGET=armv7a-linux-androideabi
         export API=19
         export AR=$TOOLCHAIN/bin/llvm-ar
-        export CC=$TOOLCHAIN/bin/$TARGET$API-clang
+        export CC=clang
         export CROSSTC=$TOOLCHAIN/bin/$TARGET$API-
         # if needed, you can try setting this as $CC or you can steal GAS AS from old ndk such as r15c and point to it there
         export AS=$TOOLCHAIN/bin/llvm-as
         export CXX=$TOOLCHAIN/bin/$TARGET$API-clang++
-        export LD=$TOOLCHAIN/bin/ld
         export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
         export STRIP=$TOOLCHAIN/bin/llvm-strip
-        make -C luajit HOST_CC=${CC} CFLAGS="-O2 -mfloat-abi=softfp -mfpu=neon -march=armv7-a" HOST_CFLAGS="-O2 -mfloat-abi=softfp -mfpu=neon -march=armv7-a" TARGET_CFLAGS="${CFLAGS}" CROSS=${CROSSTC} DESTDIR="${DEST}"
+        make -C luajit HOST_CC=${CC} STATIC_CC=${CROSSTC}${CC} DYNAMIC_CC=${CROSSTC}${CC} TARGET_LD=$TOOLCHAIN/bin/ld CFLAGS="-O2 -mfloat-abi=softfp -mfpu=neon -march=armv7-a" HOST_CFLAGS="-O2 -mfloat-abi=softfp -mfpu=neon -march=armv7-a" TARGET_CFLAGS="${CFLAGS}" CROSS=${CROSSTC} DESTDIR="${DEST}"
         ;;
     arm64-v8a)
         export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$HOST_ARCH
         export TARGET=aarch64-linux-android
         export API=21
         export AR=$TOOLCHAIN/bin/llvm-ar
-        export CC=$TOOLCHAIN/bin/$TARGET$API-clang
+        export CC=clang
         export CROSSTC=$TOOLCHAIN/bin/$TARGET$API-
         # if needed, you can try setting this as $CC or you can steal GAS AS from old ndk such as r15c and point to it there
         export AS=$TOOLCHAIN/bin/llvm-as
@@ -36,14 +35,14 @@ case "$1" in
         export LD=$TOOLCHAIN/bin/ld
         export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
         export STRIP=$TOOLCHAIN/bin/llvm-strip
-        make -C luajit HOST_CC=${CC} CFLAGS="-O2 -mfloat-abi=softfp -mfpu=neon -march=armv8-a" HOST_CFLAGS="-O2 -mfloat-abi=softfp -mfpu=neon -march=armv8-a" TARGET_CFLAGS="${CFLAGS}" CROSS=${CROSSTC} DESTDIR="${DEST}"
+        make -C luajit HOST_CC=${CC} STATIC_CC=${CROSSTC}${CC} DYNAMIC_CC=${CROSSTC}${CC} TARGET_LD=$TOOLCHAIN/bin/ld CFLAGS="-O2 -mfloat-abi=softfp -mfpu=neon -march=armv8-a" HOST_CFLAGS="-O2 -mfloat-abi=softfp -mfpu=neon -march=armv8-a" TARGET_CFLAGS="${CFLAGS}" CROSS=${CROSSTC} DESTDIR="${DEST}"
         ;;
     x86-android)
         export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$HOST_ARCH
         export TARGET=i686-linux-android
         export API=19
         export AR=$TOOLCHAIN/bin/llvm-ar
-        export CC=$TOOLCHAIN/bin/$TARGET$API-clang
+        export CC=clang
         export CROSSTC=$TOOLCHAIN/bin/$TARGET$API-
         # if needed, you can try setting this as $CC or you can steal GAS AS from old ndk such as r15c and point to it there
         export AS=$TOOLCHAIN/bin/llvm-as
@@ -51,14 +50,14 @@ case "$1" in
         export LD=$TOOLCHAIN/bin/ld
         export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
         export STRIP=$TOOLCHAIN/bin/llvm-strip
-        make -C luajit HOST_CC=${CC} CFLAGS="-O2 -mfloat-abi=softfp -march=i686" HOST_CFLAGS="-O2 -mfloat-abi=softfp -march=i686" TARGET_CFLAGS="${CFLAGS}" CROSS=${CROSSTC} DESTDIR="${DEST}"
+        make -C luajit HOST_CC=${CC} STATIC_CC=${CROSSTC}${CC} DYNAMIC_CC=${CROSSTC}${CC} TARGET_LD=$TOOLCHAIN/bin/ld CFLAGS="-O2 -mfloat-abi=softfp -march=i686" HOST_CFLAGS="-O2 -mfloat-abi=softfp -march=i686" TARGET_CFLAGS="${CFLAGS}" CROSS=${CROSSTC} DESTDIR="${DEST}"
         ;;
     x86_64-android)
         export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$HOST_ARCH
         export TARGET=x86_64-linux-android
         export API=21
         export AR=$TOOLCHAIN/bin/llvm-ar
-        export CC=$TOOLCHAIN/bin/$TARGET$API-clang
+        export CC=clang
         export CROSSTC=$TOOLCHAIN/bin/$TARGET$API-
         # if needed, you can try setting this as $CC or you can steal GAS AS from old ndk such as r15c and point to it there
         export AS=$TOOLCHAIN/bin/llvm-as
@@ -66,7 +65,7 @@ case "$1" in
         export LD=$TOOLCHAIN/bin/ld
         export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
         export STRIP=$TOOLCHAIN/bin/llvm-strip
-        make -C luajit HOST_CC=${CC} CFLAGS="-O2 -mfloat-abi=softfp" HOST_CFLAGS="-O2 -mfloat-abi=softfp" TARGET_CFLAGS="${CFLAGS}" CROSS=${CROSSTC} DESTDIR="${DEST}"
+        make -C luajit HOST_CC=${CC} STATIC_CC=${CROSSTC}${CC} DYNAMIC_CC=${CROSSTC}${CC} TARGET_LD=$TOOLCHAIN/bin/ld CFLAGS="-O2 -mfloat-abi=softfp" HOST_CFLAGS="-O2 -mfloat-abi=softfp" TARGET_CFLAGS="${CFLAGS}" CROSS=${CROSSTC} DESTDIR="${DEST}"
         ;;
     x86_64-linux)
         export CFLAGS="-O2"
