bd932a6b3a7f6e52e3403584f1140ccb58e0e522
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-11-02 15:50:28 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ case "$1" in
         rm luajit/src/Makefile
         cp -f Makefile luajit/src/Makefile
         $TOOLCHAIN/bin/ld --help
-        make -C luajit CC="${CC} -m32" HOST_CC="${CC} -m32" STATIC_CC=${CROSSTC}${CC} DYNAMIC_CC=${CROSSTC}${CC} TARGET_LD="${CROSSTC}${CC} -L $LIBSROOT" CFLAGS="-O2" HOST_CFLAGS="-O2" TARGET_CFLAGS="${CFLAGS}" CROSS=${CROSSTC} DESTDIR="${DEST}"
+        make -C luajit CC="${CC} -m32" HOST_CC="${CC} -m32" STATIC_CC=${CROSSTC}${CC} DYNAMIC_CC=${CROSSTC}${CC} TARGET_LD="${CROSSTC}${CC} -L$LIBSROOT" CFLAGS="-O2" HOST_CFLAGS="-O2" TARGET_CFLAGS="${CFLAGS}" CROSS=${CROSSTC} DESTDIR="${DEST}"
         ;;
     arm64-v8a)
         export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$HOST_ARCH
